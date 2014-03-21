ef6e4d3e4769e128d74c335639f9d79299a99c92
Kiia Korpi <kiia.korpi@aalto.fi>
2014-03-21 19:53:40 +0200

    super simple heuristic for attacking and random highestChild

--- Node.java
+++ Node.java
@@ -1,6 +1,7 @@
 package notrandombot;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import fi.zem.aiarch.game.hierarchy.Move;
 import fi.zem.aiarch.game.hierarchy.Situation;
@@ -8,72 +9,89 @@ import fi.zem.aiarch.game.hierarchy.Situation;
 //TODO Consider storing only the hash of the situation, faster comparison? Less space for sure
 
 public class Node {
-    private int score;
-    private Situation situation;
-    private Node parent;
-    private ArrayList<Node> children;
-    private Move move;
-    
-    
-    public Node(int score, Situation situation, Node parent, Move move) {
-    	this.score = score;
-    	this.situation = situation;
-    	this.parent = parent;
-    	this.children = new ArrayList<Node>();
-    	this.move = move;
-    }
-    
-    public void addChild(Node child) {
-    	this.children.add(child);
-    }
-    
-    public void setChildren(ArrayList<Node> children) {
-    	this.children = children;
-    }
-    
-    public ArrayList<Node> getChildren() {
-    	return this.children;
-    }
-    
-    public int getScore() {
-    	return this.score;
-    }
-    
-    public void setScore(int score) {
-    	this.score = score;
-    }
-    
-    public Situation getSituation() {
-    	return this.situation;
-    }
-    
-    public Move getMove() {
-    	return this.move;
-    }
-    
-    
-    public Node getHighestChild() {
-    	Node highestNode = null;
-    	for (int i = 0; i < this.children.size(); i++) {
-    		Node current = this.children.get(i);
-    		if (highestNode == null || current.score > highestNode.score) {
-    			highestNode = current;
-    		}
+	private int score;
+	private Situation situation;
+	private Node parent;
+	private ArrayList<Node> children;
+	private Move move;
+
+	public Node(int score, Situation situation, Node parent, Move move) {
+		this.score = score;
+		this.situation = situation;
+		this.parent = parent;
+		this.children = new ArrayList<Node>();
+		this.move = move;
+	}
+
+	public void addChild(Node child) {
+		this.children.add(child);
+	}
+
+	public void setChildren(ArrayList<Node> children) {
+		this.children = children;
+	}
+
+	public ArrayList<Node> getChildren() {
+		return this.children;
+	}
+
+	public int getScore() {
+		return this.score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public Situation getSituation() {
+		return this.situation;
+	}
+
+	public Move getMove() {
+		return this.move;
+	}
+
+	public Node getHighestChild() {
+    	ArrayList<Node> highestNodes = getHighestChildren();
+    	if(highestNodes.size() == 1) {
+    		return highestNodes.get(0);
+    	} else if(highestNodes.isEmpty()) {
+    		return null;
+    	}    	
+    	else {
+    		Random rand = new Random();
+    		int chosen = rand.nextInt(highestNodes.size());
+			 System.out.println("Random: " + chosen);
+    		return highestNodes.get(chosen);
     	}
-    	return highestNode;
     }
-    
-    public Node getLowestChild() {
-    	Node lowestNode = null;
-    	for (int i = 0; i < this.children.size(); i++) {
-    		Node current = this.children.get(i);
-    		if (lowestNode == null || current.score < lowestNode.score) {
-    			lowestNode = current;
-    		}
-    	}
-    	return lowestNode;
-    }
-    
-    
-    
+
+	public ArrayList<Node> getHighestChildren() {
+		int highestScore = 0;
+		ArrayList<Node> highestNodes = new ArrayList<Node>();
+		for (int i = 0; i < this.children.size(); i++) {
+			Node current = this.children.get(i);
+			if (highestNodes.isEmpty() || current.score == highestScore) {
+				highestNodes.add(current);
+			}
+			else if(current.score > highestScore) {
+				highestNodes.clear();
+				highestScore = current.score;
+				highestNodes.add(current);
+			}
+		}
+		return highestNodes;
+	}
+
+	public Node getLowestChild() {
+		Node lowestNode = null;
+		for (int i = 0; i < this.children.size(); i++) {
+			Node current = this.children.get(i);
+			if (lowestNode == null || current.score < lowestNode.score) {
+				lowestNode = current;
+			}
+		}
+		return lowestNode;
+	}
+
 }
--- NotRandomBot.java
+++ NotRandomBot.java
@@ -5,10 +5,8 @@ import java.util.BitSet;
 import java.util.List;
 import java.util.Random;
 
-
-
-
 import fi.zem.aiarch.game.hierarchy.Board;
+import fi.zem.aiarch.game.hierarchy.Coord;
 import fi.zem.aiarch.game.hierarchy.Engine;
 import fi.zem.aiarch.game.hierarchy.Move;
 import fi.zem.aiarch.game.hierarchy.Player;
@@ -17,60 +15,75 @@ import fi.zem.aiarch.game.hierarchy.Situation;
 
 public class NotRandomBot implements Player {
 	public NotRandomBot(Random rnd) {
-		this.rnd = rnd;
 		this.visitedStates = new BitSet[5];
 		this.index = 0;
 	}
-	
+
 	public void start(Engine engine, Side side) {
 		this.side = side;
 	}
-	
+
 	public Move move(Situation situation, int timeLeft) {
-	
+		System.out
+				.println("\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n!!!!!!!!!!!!!!!!!!!Move! It's time for: "
+						+ situation.getTurn());
+		System.out.println("The situation now: " + situation);
+
 		currentNode = new Node(0, situation, null, null);
 
-		expandTree(situation, currentNode);
+		expandTree(situation, currentNode, false);
 
 		for (int i = 0; i < currentNode.getChildren().size(); i++) {
 			Node child = currentNode.getChildren().get(i);
-			expandTree(situation, child);
+			expandTree(child.getSituation(), child, false);
 		}
 		Node chosen = chooseNode(situation, currentNode);
+
+		// System.out.println("Chosen situtation: " + chosen.getSituation());
+		System.out.println("Chosen move: " + chosen.getMove());
+		System.out.println("had score" + chosen.getScore());
+
 		storeEncodedState(chosen.getSituation().encode(null));
 		return chosen.getMove();
 	}
-	
-	
-	private void expandTree(Situation situation, Node node) {
 
+	private void expandTree(Situation situation, Node node, boolean log) {
 		List<Move> moves = situation.legal();
-
+		if (log)
+			System.out.println("legal moves: " + moves.size());
 		ArrayList<Node> children = new ArrayList<Node>();
 		Situation newSituation = null;
-		for (int i = 0; i <moves.size(); i++) {
+		for (int i = 0; i < moves.size(); i++) {
 			Move move = moves.get(i);
 			newSituation = situation.copyApply(move);
-			Node newNode = new Node(heuristicScore(newSituation), newSituation, node, move);
+			if (log) {
+				System.out.println(move);
+				System.out.println(newSituation);
+
+				System.out.println("_______------!!!!HEURISTIC SCORE: "
+						+ heuristicScore(newSituation));
+			}
+			Node newNode = new Node(heuristicScore(newSituation), newSituation,
+					node, move);
 			children.add(newNode);
 		}
 		node.setChildren(children);
 		childEffect(node);
 	}
-	
+
 	private Node chooseNode(Situation situation, Node node) {
 		Side turn = situation.getTurn();
-		if (turn != side) {
+		if (turn == side) {
 			return node.getHighestChild();
 		} else {
 			return node.getLowestChild();
 		}
 	}
-	
+
 	private int heuristicScore(Situation situation) {
-	
+
 		BitSet hashed = situation.encode(null);
-		for (int i = 0; i<this.visitedStates.length; i++) {
+		for (int i = 0; i < this.visitedStates.length; i++) {
 			if (hashed.equals(visitedStates[i])) {
 				return Integer.MIN_VALUE;
 			}
@@ -78,35 +91,47 @@ public class NotRandomBot implements Player {
 		Board board = situation.getBoard();
 		int score = 0;
 		Iterable<Board.Square> ownPieces = board.pieces(this.side);
-	
 		for (Board.Square s : ownPieces) {
-			score++;
+			score += s.getPiece().getValue();
 			ownPieces.iterator().next();
 		}
-	
+
 		Iterable<Board.Square> enemyPieces = board.pieces(this.side.opposite());
 		for (Board.Square s : enemyPieces) {
-			score--;
+			score -= s.getPiece().getValue();
 			enemyPieces.iterator().next();
 		}
 
+		//this side attacks
+		if (situation.getAttacker(this.side) != null) {
+			score += 5 * (board.get(situation.getTarget(this.side)).getValue());
+		}
+		//opposite side attacks
+		if (situation.getAttacker(this.side.opposite()) != null) {
+			score -= 5 * (board.get(situation.getTarget(this.side.opposite()))
+					.getValue());
+		}
+		
+		//if MoveType.PASS ----- 
+
 		return score;
 	}
-	
+
 	private void childEffect(Node node) {
+		int newScore = node.getScore();
 		Node childNode = chooseNode(node.getSituation(), node);
-		int newScore = (int)(((double)childNode.getScore() + node.getScore()) / 2);
+		if (childNode != null) {
+			newScore = (int) (((double) childNode.getScore() + node
+					.getScore()) / 2);
+		}
 		node.setScore(newScore);
 	}
-	
+
 	private void storeEncodedState(BitSet encoded) {
 		this.visitedStates[index] = encoded;
 		index = (index < MAXINDEX) ? index + 1 : 0;
 	}
-	
-	
-	
-	private Random rnd;
+
 	private Node currentNode;
 	private Side side;
 	private BitSet[] visitedStates;
